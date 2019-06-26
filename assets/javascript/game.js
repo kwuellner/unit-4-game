65b6e760dff46cfd60b5963949e7601404f841af
Kyle Wuellner <rockstar81@comcast.net>
2019-06-25 23:57:29 -0500

    redid js from scratch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
 
-    var goalNumber = [];
-    var totalWins = 0;
-    var totalLosses = 0;
-    var crystalNum1 = [];
-    var crystalNum2 = [];
-    var crystalNum3 = [];
-    var crystalNum4 = [];
+    var wins = 0;
+    var losses = 0;
+    var yourTotal = 0;
 
     var goalNumber = Math.floor((Math.random() * 120) + 19)
     var crystalNum1 = Math.floor((Math.random() * 12) + 1)
@@ -14,13 +10,88 @@ $(document).ready(function () {
     var crystalNum3 = Math.floor((Math.random() * 12) + 1)
     var crystalNum4 = Math.floor((Math.random() * 12) + 1)
 
-    // console.log(goalNumber);
-    // console.log(crystalNum1);
-    // console.log(crystalNum2);
-    // console.log(crystalNum3);
-    // console.log(crystalNum4);
+    $('#totalWins').text(wins);
+    $('#totalLosses').text(losses);
 
-    $("#number-to-guess").text(goalNumber);
+    function reset() {
+        goalNumber = Math.floor((Math.random() * 120) + 19);
+        console.log(goalNumber);
+        $('#numberToGuess').text(goalNumber);
+        crystalNum1 = Math.floor((Math.random() * 12) + 1);
+        crystalNum2 = Math.floor((Math.random() * 12) + 1);
+        crystalNum3 = Math.floor((Math.random() * 12) + 1);
+        crystalNum4 = Math.floor((Math.random() * 12) + 1);
+        yourTotal = 0;
+        $('#totalScore').text(yourTotal);
+    }
+    console.log(goalNumber);
 
+    function win() {
+        alert("Congratulations!");
+        wins++;
+        $('#totalWins').text(wins);
+        reset()
+    }
+    function lost() {
+        alert("Sorry, better luck next time!");
+        losses++;
+        $('#totalLosses').text(losses);
+        reset()
+    }
 
+    $('#one').on('click', function () {
+        yourTotal = yourTotal + crystalNum1;
+        console.log("New yourTotal =" + yourTotal);
+        $('#totalScore').text(yourTotal);
+
+        if (yourTotal == goalNumber) {
+            win();
+        }
+
+        else if (yourTotal > goalNumber) {
+            lost();
+        }
+    })
+
+    $('#two').on('click', function () {
+        yourTotal = yourTotal + crystalNum2;
+        console.log("New yourTotal =" + yourTotal);
+        $('#totalScore').text(yourTotal);
+
+        if (yourTotal == goalNumber) {
+            win();
+        }
+
+        else if (yourTotal > goalNumber) {
+            lost();
+        }
+    })
+
+    $('#three').on('click', function () {
+        yourTotal = yourTotal + crystalNum3;
+        console.log("New yourTotal =" + yourTotal);
+        $('#totalScore').text(yourTotal);
+
+        if (yourTotal == goalNumber) {
+            win();
+        }
+
+        else if (yourTotal > goalNumber) {
+            lost();
+        }
+    })
+
+    $('#four').on('click', function () {
+        yourTotal = yourTotal + crystalNum4;
+        console.log("New yourTotal =" + yourTotal);
+        $('#totalScore').text(yourTotal);
+
+        if (yourTotal == goalNumber) {
+            win();
+        }
+
+        else if (yourTotal > goalNumber) {
+            lost();
+        }
+    })
 });
